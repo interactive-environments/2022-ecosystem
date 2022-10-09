0f91d3465f959129536adcc51d6ff3e596ca4825
VivianHN <72889473+VivianHN@users.noreply.github.com>
2022-10-09 23:39:39 +0200

    allow slider object to pass port
    slider has a deafult port A2, but its possible to give another port as parameter

--- lib/components/slider.py
+++ lib/components/slider.py
@@ -6,8 +6,11 @@ from analogio import AnalogOut
 import pwmio
 
 class Slider():
-    def __init__(self):
-        self.slider = AnalogIn(board.A2)
+    def __init__(self, port=None):
+        if port == None:
+            self.slider = AnalogIn(board.A2)
+        else:
+            self.slider = AnalogIn(port)
 
     def sense(self):
         return self.slider.value
