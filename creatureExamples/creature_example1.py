954bd5e95c627b5b6cf8b6c544e405dec03fc6e4
VivianHN <72889473+VivianHN@users.noreply.github.com>
2022-10-10 11:42:01 +0200

    added example 1
    this code simulate 3 states: 
    - happy: green light and no servo movement
    - neutral: white light and slow servo movement
    - angry: red light and fast servo movement

---
+++ creatureExamples/creature_example1.py
@@ -0,0 +1,98 @@
+from components.button import Button
+from components.buzzer import Buzzer
+from components.led import LED
+from components.servo_motor import Servo
+from components.slider import Slider
+import board
+import random
+
+button = Button()
+buzzer = Buzzer()
+led = LED(1)
+servo = Servo()
+slider = Slider(port=board.A4)
+
+increase = True
+led_power = 255
+color = (1, 0, 0)
+direction = "increase"
+servo_step = 0
+prev_button_value = False
+
+
+
+class Creature:
+
+    def __init__(self):
+        self.ecosystem = None
+
+    def message(self, msg):
+        global color, servo_step
+        print("recieved: " + str(msg))
+        if msg == "ping": #simulate a neutral statee with white light and slow servo
+            color = (1,0,0)
+            servo_step = 5
+
+        # Send the ping message when we recive pong
+        if msg == "pong": #simulate a happy state with green light and no servo
+            color = (0, 1, 0)
+            servo_step = 0
+            # There is a 25% change that we will repley with ping
+            if random.randint(0,3) == 3:
+                self.ecosystem.send_message("ping")
+
+        if msg == "pow": #simulate an angry state with white light and fast servo
+            color = (200, 200, 200)
+            servo_step = 1
+
+    def sense(self):
+        if button.sense() == True:
+            return True
+
+    # One iteration of the creatures main loop
+    def loop(self):
+        global increase, led_power, color, direction, servo_step, prev_button_value
+
+        # If the button in pressed then send the message based on the slider value
+        if self.sense() and not prev_button_value:
+            if(slider.sense() > 45000):
+                self.ecosystem.send_message("ping")
+            elif (slider.sense() > 23000):
+                self.ecosystem.send_message("pong")
+            else:
+                self.ecosystem.send_message("pow")
+            prev_button_value = True
+        elif not self.sense():
+            prev_button_value = False
+
+
+        # increase or decease the brightness by 1 every loop.
+        if increase:
+            led_power += 1
+            if led_power > 255:
+                led_power = 255
+                increase = False
+        else:
+            led_power -= 1
+            if led_power < 0:
+                led_power = 0
+                increase = True
+
+        if direction == "increase":
+            new_angle = servo.lastAngle + servo_step
+        elif direction == "decrease":
+            new_angle = servo.lastAngle - servo_step
+
+        if (new_angle < 0):
+            new_angle = 0
+            direction = "increase"
+        elif (new_angle > 180):
+            new_angle = 180
+            direction = "decrease"
+        servo.update(new_angle)
+        servo.lastAngle = new_angle
+
+
+        # show the led color
+        actual_color = tuple([led_power * c for c in color])
+        led.update_full_color(actual_color)
